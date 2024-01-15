7a4872291bd230e66a7c80964d9611c9551a3061
ccocjin <chrisangelococjin@gmail.com>
2024-01-15 21:51:46 +0800

    fix try

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function toggleMenu(){
-    //targeting the two elements and when clicked it will
-    //add or remove the open class
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+function toggleMenu() {
+  //targeting the two elements and when clicked it will
+  //add or remove the open class
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
